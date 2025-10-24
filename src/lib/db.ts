f3c5243f886168ec4db439b463f098998c929f53
Jeffery Samatthiya <jsamatthiya@dons.usfca.edu>
2025-10-24 16:03:18 -0700

    Refactor db.ts for better readability

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-export const db = globalThis.__db || new PrismaClient();
-if (process.env.NODE_ENV !== "production") (globalThis as any).__db = db;
+
+export const db =
+  (globalThis as any).__db || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  (globalThis as any).__db = db;
+}
